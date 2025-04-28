59f46e1fe681158f33eb0e4910de34013a9f3bdc
Daniela <pao.dan.cha.con@gmail.com>
2025-04-28 12:54:14 -0400

    poke version 4 completed

--- poke_game/poke.py
+++ poke_game/poke.py
@@ -1,9 +1,9 @@
-# Poke the Dots V3
+# Poke the Dots V4
 # This is a graphical game where two dots move around
-# the screen, bouncing off the edges
+# the screen, bouncing off the edges. The usser tries 
+
 from uagame import Window
 from random import randint
-import pygame
 from pygame import QUIT, Color, MOUSEBUTTONUP, KEYDOWN, K_p
 from pygame.time import Clock, get_ticks
 from pygame.event import get as get_events
@@ -11,129 +11,10 @@ from pygame.draw import circle as draw_circle
 
 def main():
     # create the window
-    window =create_window()
-    game = create_game(window)
-    # play game
-    play_game(game)
-    # close window
-    window.close()
-
-
-def create_window():
-    # Create a window for the game, open it, and return it
-    window = Window('Poke the Dots', 500, 400)
-    window.set_font_size(64)
-    window.set_font_color('white')
-    window.set_bg_color('black')
-    return window
-
-def create_game(window,):
-    # Create a Game object for poke the dots
     game = Game()
-    game.window = window
-    game.frame_rate = 90
-    game.close_selected = False
-    game.clock = Clock()
-    game.small_dot = create_dot('red', [50,75], 30, [1,2], window)
-    game.big_dot = create_dot('blue', [200,100], 40, [2,1], window)
-    randomize_dot(game.small_dot)
-    randomize_dot(game.big_dot)
-    game.score = 0
-    return game
+    # play game
+    game.play()
 
-def create_dot(color, center, radius, speed, window):
-    dot = Dot()
-    dot.color = color
-    dot.center = center
-    dot.radius = radius
-    dot.velocity = speed
-    dot.window = window
-    return dot
-
-def play_game(game):
-    
-    # while not player has selected close
-    while not game.close_selected:
-        # play frame
-        #   handle events
-        handle_events(game)
-        #   draw game
-        draw_game(game)
-        #   update game
-        update_game(game)
-    
-
-
-def handle_events(game):
-    
-    event_list = get_events()
-    # for event in event_list
-    for event in event_list:
-        #   handle one event
-        #       if event category equals close
-        if event.type == QUIT:
-            # remember play has selected close
-            game.close_selected = True
-        
-        elif event.type == MOUSEBUTTONUP:
-            handle_mouse_up(game)
-
-        elif event.type == KEYDOWN:
-            if event.key == K_p:
-                handle_mouse_up(game)
-
-
-def handle_mouse_up(game):
-    randomize_dot(game.small_dot)
-    randomize_dot(game.big_dot)
-
-def draw_game(game):
-    game.window.clear()
-    draw_score(game)
-    # draw small dot
-    # draw big dot
-    draw_dot(game.small_dot)
-    draw_dot(game.big_dot)
-    # update display
-    game.window.update()
-
-def draw_score(game):
-    string = 'Score: ' +str(game.score)
-    game.window.draw_string(string, 0, 0)
-
-def update_game(game):
-
-    # move small dot
-    move_dot(game.small_dot)
-    # move big dot
-    move_dot(game.big_dot)
-    # control frame rate
-    game.clock.tick(game.frame_rate)
-    game.score =get_ticks() // 1000
-
-def draw_dot(dot):
-    surface = dot.window.get_surface()
-    color = Color(dot.color)
-    draw_circle(surface, color, dot.center, dot.radius, 3)
-
-def move_dot(dot):
-    size = (dot.window.get_width(), dot.window.get_height())
-    # for index in sequence 0 to 1
-    for index in range(0,2):
-        # update center at index
-        #   add velocity at index to center at index
-        dot.center[index] = dot.center[index] + dot.velocity[index]
-        #   if dot edge outside window
-        if (dot.center[index] < dot.radius) or (dot.center[index] + dot.radius > size[index]):
-        #if ((center[index] == radius) or (center[index] == size[index])):
-            # neagte velocity at index
-            dot.velocity[index] = -dot.velocity[index]
-
-
-def randomize_dot(dot):
-    size = (dot.window.get_width(), dot.window.get_height())
-    for index in range (0, 2):
-        dot.center[index] = randint(dot.radius, size[index] - dot.radius)
 # class game
 class Game:
     # An object in this class represents a complete game
@@ -143,8 +24,89 @@ class Game:
     # - clock
     # - small_dot
     # - big_dot
-    pass
+    def __init__(self):
+        self._window = Window('Poke the Dots', 500, 400)
+        self._adjust_window()
+        self._frame_rate = 90
+        self._close_selected = False
+        self._clock = Clock()
+        self._small_dot = Dot('red', [50,75], 30, [1,2], self._window)
+        self._big_dot = Dot('blue', [200,100], 40, [2,1], self._window)
+        self._small_dot.randomize()
+        self._big_dot.randomize()
+        self._score = 0
 
+
+    def _adjust_window(self):
+        self._window.set_font_name('ariel')
+        self._window.set_font_size(64)
+        self._window.set_font_color('white')
+        self._window.set_bg_color('black')
+
+    
+    def play(self):
+        # while not player has selected close
+        while not self._close_selected:
+            # play frame
+            #   handle events
+            self.handle_events()
+            #   draw game
+            self.draw()
+            #   update game
+            self.update()
+        self._window.close()
+
+
+    def handle_events(self):
+        event_list = get_events()
+        # for event in event_list
+        for event in event_list:
+            self.handle_one_event(event)
+
+
+    def handle_one_event(self, event):
+        #   handle one event
+        #       if event category equals close
+        if event.type == QUIT:
+            # remember play has selected close
+            self._close_selected = True
+        
+        elif event.type == MOUSEBUTTONUP:
+            self.handle_mouse_up(event)
+
+
+    def handle_mouse_up(self, event):
+        self._small_dot.randomize()
+        self._big_dot.randomize()
+
+
+    def draw(self):
+        self._window.clear()
+        self.draw_score()
+        # draw small dot
+        # draw big dot
+        self._small_dot.draw()
+        self._big_dot.draw()
+        # update display
+        self._window.update()
+
+
+    def update(self):
+
+        # move small dot
+        self._small_dot.move()
+        # move big dot
+        self._big_dot.move()
+        # control frame rate
+        self._clock.tick(self._frame_rate)
+        self._score = get_ticks() // 1000
+        
+
+    def draw_score(self):
+        string = 'Score: ' +str(self._score)
+        self._window.draw_string(string, 0, 0)
+
+    
 # class dot
 class Dot:
     # An object in this class represents a colored circle that can move
@@ -153,6 +115,36 @@ class Dot:
     # - radius
     # - velocity
     # - window
-    pass
+    def __init__(self, color, center, radius, velocity, window):
+        self._color = color
+        self._center = center
+        self._radius = radius
+        self._velocity = velocity
+        self._window = window
 
+
+    def move(self):
+        size = (self._window.get_width(), self._window.get_height())
+        # for index in sequence 0 to 1
+        for index in range(0,2):
+            # update center at index
+            #   add velocity at index to center at index
+            self._center[index] = self._center[index] + self._velocity[index]
+            #   if dot edge outside window
+            if (self._center[index] < self._radius) or (self._center[index] + self._radius > size[index]):
+            #if ((center[index] == radius) or (center[index] == size[index])):
+                # neagte velocity at index
+                self._velocity[index] = -self._velocity[index]
+
+
+    def draw(self):
+        surface = self._window.get_surface()
+        color = Color(self._color)
+        draw_circle(surface, color, self._center, self._radius, 3)
+
+
+    def randomize(self):
+        size = (self._window.get_width(), self._window.get_height())
+        for index in range (0, 2):
+            self._center[index] = randint(self._radius, size[index] - self._radius)
 main()
